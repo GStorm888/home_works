47e5b676990ef675972bb8c0e301a44fbf32e436
GStorm888 <denchikevstr@icloud.com>
2024-02-10 18:11:30 +0300

    создал рабочую функцию min

--- max.py
+++ max.py
@@ -5,6 +5,17 @@ def max(first, second):
         max = second
     elif first == second:
         max = first 
+    return max
 
+
+
+def min(first, second):
+    if first > second:
+        min = first
+    elif first < second:
+        min = second
+    elif first == second:
+        min = first 
+    return min
 
 
